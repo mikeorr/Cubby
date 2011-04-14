86d71094d85f36bbf3b10b6bffc51b6d29244e8e
Mike Orr <sluggoster@gmail.com>
2011-04-13 21:23:13 -0700

    Download 100 pages and save them in files.

--- dl-bb-proto2.py
+++ dl-bb-proto2.py
@@ -1,55 +1,41 @@
-import datetime
+"""Fetch 100 pages using httplib2."""
+
+# Based on an eventlet recipe: 
+# http://eventlet.wordpress.com/page/2/
+# Actually not, I couldn't get it to work.
+
+import os
 import re
+import time
 import urllib
 
-from BeautifulSoup import BeautifulSoup, NavigableString, Tag
 import httplib2
 
-FIELD_RX = re.compile("^[A-Za-z ]+:")
-
-def get_page():
-    h = httplib2.Http()
-    url = "http://bodyspace.bodybuilding.com/photos/view-latest/all"
-    status, content = h.request(url)
-    f = open("page1.html", "w")
-    f.write(content)
-    f.close()
-    return content
+LATEST_PICTURES_PAGES = 100
 
 def main():
-    #page = get_page()
-    f = open("page1.html")
-    html = f.read()
-    f.close()
-    soup = BeautifulSoup(html)
-    nodes = soup.findAll("div", "boom-three-column")
-    print len(nodes), "nodes"
-    print
-    photo_ids = []
-    for i, node in enumerate(nodes):
-        print "Node #{0}.".format(i)
-        top = node.find("div", "top")
-        middle = node.find("div", "middle")
-        photo_url = top.a["href"]
-        thumb_url = top.img["src"]
-        user_url = username = date = None
-        for div in middle.findAll("div"):
-            first = div.contents[0]
-            if isinstance(first, NavigableString):
-                if first.startswith("User:"):
-                    user_url = div.a["href"]
-                    username = user_url.rsplit("/", 1)[1]
-                elif first.startswith("Date Taken:"):
-                    date = datetime.datetime.strptime(first, "Date Taken: %b %d, %Y").date()
-        print "photo_url = ", photo_url
-        print "thumb_url = ", thumb_url
-        print "user_url = ", user_url
-        print "username = ", username
-        print "date = ", date
-        print
-        
-
-
-    
+    http = httplib2.Http()
+    url = "http://bodyspace.bodybuilding.com/photos/view-latest/all"
+    dst_dir = "pages"
+    if not os.path.exists(dst_dir):
+        os.path.makedirs(dest_dir)
+    begin = time.time()
+    for i in xrange(LATEST_PICTURES_PAGES):
+        page = i + 1
+        start = 19 * i
+        qs = urllib.urlencode({"start": start})
+        u = "{0}?{1}".format(url, qs)
+        page_begin = time.time()
+        headers, content = http.request(u)
+        elapsed = time.time() - page_begin
+        print "... page {0} ({1:0.4f} seconds)".format(page, elapsed)
+        filename = "page-{0:03}.html".format(page)
+        p = os.path.join(dst_dir, filename)
+        f = open(p, "w")
+        f.write(content)
+        f.close()
+    elapsed = time.time() - begin
+    msg = "Finished {0} pages in {1:0.4f} seconds"
+    print msg.format(LATEST_PICTURES_PAGES, elapsed)
 
 if __name__ == "__main__":  main()
